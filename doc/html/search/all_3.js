a6b91785a792cd4917a0dbaff13481e7f493b9d6
pvthuyet <pvthuyet@gmail.com>
2021-07-22 07:10:56 +0700

    change logo

--- doc/html/search/all_3.js
+++ doc/html/search/all_3.js
@@ -2,6 +2,6 @@ var searchData=
 [
   ['server_4',['server',['../classserver.html',1,'']]],
   ['service_5fmanager_5',['service_manager',['../classservice__manager.html',1,'']]],
-  ['sgeyes2_6',['SGEyes2',['../md__r_e_a_d_m_e.html',1,'']]],
+  ['sgeyes2_6',['SGEyes2',['../md__d__projects__s_g_eyes2__r_e_a_d_m_e.html',1,'']]],
   ['start_7',['start',['../classwatcher__manager.html#aefab133e8115bb218b93de97094b3f38',1,'watcher_manager']]]
 ];
